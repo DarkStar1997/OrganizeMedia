ff046a0070e5d48e1468f206848dcc9a86b2f4e2
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2022-03-08 17:27:38 +0530

    Initial prototype working

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstdlib>
 #include <fmt/core.h>
 #include <filesystem>
 #include <chrono>
@@ -10,14 +11,12 @@
 std::vector<std::string> weekday = {"sun", "mon", "tue", "wed", "thur", "fri", "sat"};
 std::vector<std::string> month = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"};
 
-std::string formatted_last_write_time(std::filesystem::file_time_type const& ftime)
+std::filesystem::path get_output_dir(std::filesystem::file_time_type const& ftime, const std::filesystem::path output_path)
 {
     std::time_t cftime = std::chrono::system_clock::to_time_t(
             std::chrono::file_clock::to_sys(ftime));
-    //auto res = std::string(std::asctime(std::localtime(&cftime)));
-    //return res.substr(0, res.length() - 1);
     std::tm * timestamp = std::localtime(&cftime);
-    return fmt::format("{} {} {}", timestamp->tm_mday, month[timestamp->tm_mon], weekday[timestamp->tm_wday]);
+    return output_path / std::to_string(1900 + timestamp->tm_year) / month[timestamp->tm_mon] / std::to_string(timestamp->tm_mday);
 }
 
 int main(int argc, char **argv)
@@ -36,7 +35,14 @@ int main(int argc, char **argv)
     if(!(cmdl({"i", "input"}) >> input))
         fmt::print("Provide input directory with -i or --input\n");
     else
+    {
+        if(!std::filesystem::exists(input))
+        {
+            fmt::print("Input directory {} not present\n", input);
+            exit(0);
+        }
         fmt::print("Input directory set to {}\n", input);
+    }
     
     if(!(cmdl({"o", "output"}) >> output))
         fmt::print("Provide output directory with -o or --output\n");
@@ -44,16 +50,21 @@ int main(int argc, char **argv)
         fmt::print("Output directory set to {}\n", output);
 
     std::vector<std::string> extensions = {".json", ".cpp", ".cache"};
-    size_t count = 0;
+    size_t count = 0, duplicates = 0;
 
     for(const auto& dir_entry : std::filesystem::recursive_directory_iterator(input))
     {
         if(std::find(extensions.begin(), extensions.end(), dir_entry.path().extension()) != extensions.end())
         {
-            fmt::print("[{}] - {}\n", dir_entry.path().string(), formatted_last_write_time(std::filesystem::last_write_time(dir_entry)));
-            count++;
+            auto output_path = get_output_dir(std::filesystem::last_write_time(dir_entry), std::filesystem::path(output));
+            if(!std::filesystem::exists(output_path))
+                std::filesystem::create_directories(output_path);
+            uint8_t isUnique = std::filesystem::copy_file(dir_entry.path(), output_path / dir_entry.path().filename(), std::filesystem::copy_options::skip_existing);
+            count += isUnique;
+            if(!isUnique)
+                duplicates++;
         }
     }
 
-    fmt::print("{} files found\n", count);
+    fmt::print("{} New files found\n {} Duplicate files\n", count, duplicates);
 }
